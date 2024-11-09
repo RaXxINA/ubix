1b5721e4045549d666cefe5b2fddb5b17eaaa826
RaXxINA <161144359+RaXxINA@users.noreply.github.com>
2024-11-09 14:17:19 +0100

    Create  guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,21 @@
+function gameofguessing(){
+    numoffiles=$(pwd | ls | wc -l)
+    while true;
+    do
+        echo "enter your guess :"
+        read  number
+        if [ $number -lt $numoffiles ]
+        then
+            echo "Your guess is less than the number of files"
+        elif [ $number -gt $numoffiles ]
+        then
+            echo "Your guess is more than the number of files"
+        else
+            echo "Your guess is correct!"
+        break;
+        fi
+    done
+}
+
+echo "Guess how many files are in the current directory ?"
+gameofguessing
